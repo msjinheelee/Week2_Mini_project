30d02a07b821684466dbd8693907f3393025b2fc
JINHEE LEE <ms.jinhee.lee@gmail.com>
2022-04-18 14:00:19 +1000

    updates w homework links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 
-alert("Hello, you!");
+
+let name = prompt("Hello, What is your name?");
+alert("Hello, " + name + "!");
+
+const info = require('info');
+
+// TODO: Add comments to explain each of the three arguments of appendFile() \n means new line
+info.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
+  // TODO: Describe how this ternary operator works
+  err ? console.error(err) : console.log('Commit logged!')
+);
